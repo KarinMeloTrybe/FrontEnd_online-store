e7c230c2178201f14f9a900e718c84ad2b0108b3
RobertoHortaFilho <robertohortafilho@gmail.com>
2022-08-05 16:51:33 -0300

    feat(Home): Criação da page Home
    renderizaçao do input de busca e condicional da lista de produtos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
+import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" />
+          <Route path="/" component={ Home } />
         </Switch>
       </BrowserRouter>
     );
