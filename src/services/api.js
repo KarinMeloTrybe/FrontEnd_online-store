095f561f5d48997de6db0211f5eb42e08a651648
FelipeBresciani <fe968000322@gmail.com>
2022-08-05 17:04:46 -0300

    feat: implementação da função getCategories() e getProductsFromCategoryAndQuery()

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const json = url.json();
+  return json;
 }
-
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (categoryId) {
+    const requisition = await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const json = requisition.json();
+    return json;
+  } if (query) {
+    const requisitionQuery = await fetch(` https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const jsonQuery = requisitionQuery.json();
+    return jsonQuery;
+  } if (categoryId && query) {
+    const requisitionCategoryIdAndQ = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const jsonCategoryIdAndQ = requisitionCategoryIdAndQ.json();
+    return jsonCategoryIdAndQ;
+  }
 }
