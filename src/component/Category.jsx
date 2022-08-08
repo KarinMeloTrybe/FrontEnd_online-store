8212bba78622b1526f6ff634826f72cbb63ae3fb
RobertoHortaFilho <robertohortafilho@gmail.com>
2022-08-08 18:15:54 -0300

    feat(Page Home): pesquisa de categorias
    logica do fetch para bater na api com a categoria dos produtos

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,40 +1,27 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import PropTypes from 'prop-types';
 
 class Category extends Component {
-    state = {
-      category: [],
-    }
-
-    async componentDidMount() {
-      const categories = await getCategories();
-      this.setState({ category: categories });
-    }
-
-    render() {
-      const { category } = this.state;
-      return (
-        <form>
-          {
-            category.map((categoryItem) => (
-              <label
-                key={ categoryItem.id }
-                data-testid="category"
-                htmlFor={ categoryItem.name }
-              >
-                {' '}
-                {categoryItem.name}
-                <input
-                  type="radio"
-                  name={ categoryItem.name }
-                  value={ categoryItem.name }
-                />
-              </label>
-            ))
-          }
-        </form>
-      );
-    }
+  render() {
+    const { fetchCategory, name, id } = this.props;
+    return (
+      <button
+        type="button"
+        data-testid="category"
+        name="category"
+        onClick={ fetchCategory }
+        value={ id }
+      >
+        { name }
+      </button>
+    );
+  }
 }
+
+Category.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  fetchCategory: PropTypes.func.isRequired,
+};
 
 export default Category;
